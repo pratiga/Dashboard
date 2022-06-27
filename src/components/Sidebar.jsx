12bc939a9b62cd6382450c68e113544f5bfc83d0
Pratima <gaitrisanu2468@gmail.com>
2022-06-27 16:52:05 +0545

    add sidebar
    Signed-off-by: Pratima <gaitrisanu2468@gmail.com>

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,44 @@
+import React,{useState} from 'react'
+import Logo from '../imgs/logo.png'
+import  './Sidebar.css';
+import { SidebarData } from './Data/data';
+import {UilSignOutAlt} from '@iconscout/react-unicons';
+
+const Sidebar = () => {
+  const [selected, setselected] = useState(0)
+  return (
+    <>
+    <div className="Sidebar">
+        <div className="logo">
+        <img src={Logo} alt=""/>
+        <span>
+            Sh<span>o</span>ps
+        </span>
+        </div>
+   
+    
+    {/* // menu */}
+    <div className='menu'>
+      {SidebarData.map((item, index)=> {
+        return(
+          <div className={selected===index?'menuItem active': 'menuItem'}
+          key={index}
+          onClick={()=> setselected(index)}
+          >
+            <item.icon/>
+            <span>
+              {item.heading}
+            </span>
+          </div>
+        )
+      })}
+      <div className='menuItem'>
+        <UilSignOutAlt/>
+      </div>
+    </div>
+    </div>
+    </>
+  )
+}
+
+export default Sidebar
