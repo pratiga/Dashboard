900506c02b335a9def1797d91da378816e76c51b
Pratima <gaitrisanu2468@gmail.com>
2022-06-28 22:00:05 +0545

    add route
    Signed-off-by: Pratima <gaitrisanu2468@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import logo from './logo.svg';
+
 import './App.css';
+import MainDash from './components/MainDash/MainDash';
+import Sidebar from './components/Sidebar';
+
 
 function App() {
   return (
     <div className="App">
+      <div className='AppGlass'>
+      <Sidebar/>
+      <MainDash /> 
+      <div></div>
+      <div></div>
+      <div></div>
       
+      
+      </div>
     </div>
   );
 }
