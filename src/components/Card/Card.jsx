26a7f52694f428b9d9673d973fd25e3b2c372692
Pratima <gaitrisanu2468@gmail.com>
2022-06-28 21:58:07 +0545

    add card
    Signed-off-by: Pratima <gaitrisanu2468@gmail.com>

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,52 @@
+import React,{useState} from 'react'
+import { AnimateSharedLayout } from 'framer-motion'
+import { CircularProgressbar } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
+import './Card.css'
+
+const Card = (props) => {
+    const [expanded, setExpanded] = useState(false)
+  return (
+    <>
+    <AnimateSharedLayout>
+      {
+        expanded? (
+          'Expended'
+        ):
+        <CompactCard param = {props} setExpanded={()=>setExpanded}/>
+      }
+    
+    </AnimateSharedLayout>
+  </>
+  )
+}
+
+//compact Card 
+function CompactCard ({param}){
+  const Png = param.png;
+  return(
+    <div className="CompactCard"
+    style={{
+        background: param.color.backGround,
+        boxShadow: param.color.boxShadow, 
+    }}
+    
+    >
+    
+      <div className='radialBar'>
+       <CircularProgressbar
+       value={param.barValue}
+       text={`${param.barValue}%`}
+       />
+       <span>{param.title}</span>
+      </div>
+      <div className="detail">
+        <Png/>
+        <span>${param.value}</span>
+        <span>Last 24 hours</span>
+      </div>
+    </div>
+  )
+}
+
+export default Card
