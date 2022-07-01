1b6f757cf38ac67afe7307027306190b7b84b6be
Pratima <gaitrisanu2468@gmail.com>
2022-07-01 10:07:48 +0545

    data of customer
    Signed-off-by: Pratima <gaitrisanu2468@gmail.com>

--- src/Data/data.js
+++ src/Data/data.js
@@ -10,6 +10,10 @@ import {
     
 } from "@iconscout/react-unicons";
 
+import img1 from '../imgs/img1.jpg'
+import img2 from '../imgs/img2.jpg'
+import img3 from '../imgs/img3.jpg'
+
 // Sidebar Data 
 export const SidebarData = [
     {
@@ -85,4 +89,27 @@ export const CardsData = [
              },
          ],      
     },
+]
+
+// Recent update Card Data
+export const UpdatesData = [
+    {
+        img: img1,
+        name: "Andrew Thomas",
+        noti: "has ordered Apple smart watch 2500mh battery",
+        time: "25 seconds ago",
+    },
+    {
+        img: img2,
+        name: "james Bond",
+        noti:"has received Samsung gadget for charging battery",
+        time:"30 minute ago",
+    },
+    {
+        img: img3,
+        name:"Iron Man",
+        noti:"has ordered Apple smart watch, samsung Gear 2500mh battery",
+        time:"2 hours ago",
+    },
+
 ]
